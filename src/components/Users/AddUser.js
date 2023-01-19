16aaa96fbea6fe5c675118c64aafc71a4c8dfd4b
wantop <wantop@jbnu.ac.kr>
2023-01-19 19:08:39 +0900

    feat : Card UI

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
-const AddUser = props => {
-    const addUserHandler = (event) => {
-        event.preventdefault();
-    }
-    return(
-        <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>username</label>
-            <input type="text" id="username" />
-            <label htmlFor='age'>age</label>
-            <input type="number" id="age" />
-            <button type='submit'>Add User</button>
-        </form>
-    );
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventdefault();
+  };
+  return (
+    <Card className = {classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">age</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;

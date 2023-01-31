51f09788a586695388a2a867a3d3fa841af2c984
wantop <wantop@jbnu.ac.kr>
2023-01-31 23:18:34 +0900

    feat : 16-working-with-forms

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,74 @@
+import useInput from "../hooks/use-input";
+
 const SimpleInput = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    valueBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    valueBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+    console.log(enteredEmail);
+
+    resetNameInput();
+    resetEmailInput();
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={onSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          type="text"
+          id="name"
+          value={enteredName}
+        />
+        {nameInputHasError && (
+          <p className="error-text">이름은 공백일 수 없습니다.</p>
+        )}
+
+        <label htmlFor="email">Your Email</label>
+
+        <input
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          type="email"
+          id="email"
+          value={enteredEmail}
+        />
+        {emailInputHasError && (
+          <p className="error-text">올바른 이메일을 입력해주세요.</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );

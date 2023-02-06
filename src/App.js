0862f7ce485da86e5f910587b7a70284ed24bfd5
wantop <wantop@jbnu.ac.kr>
2023-02-06 18:37:10 +0900

    feat : 20-react-router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import { Redirect, Route, Switch } from "react-router-dom";
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./components/ProductDetail";
+import { Route, Switch, Redirect } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,11 +1,13 @@
 import { Route } from "react-router-dom";
 const Welcome = () => {
-  return <section>
-    <h1>Welcome</h1>
-    <Route path="/welcome/new-user">
-      <p>Welcome, new user!</p>
-    </Route>
+  return (
+    <section>
+      <h1>Welcome</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user!</p>
+      </Route>
     </section>
+  );
 };
 
 export default Welcome;

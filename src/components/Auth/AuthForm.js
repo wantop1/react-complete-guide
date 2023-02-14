dec674f68d48a8478792cf3703ecbb6b58b37175
wantop <wantop@jbnu.ac.kr>
2023-02-14 18:47:34 +0900

    feat : 22-authentication

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,34 +1,89 @@
-import { useState } from 'react';
-
-import classes from './AuthForm.module.css';
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import classes from "./AuthForm.module.css";
+import { useHistory } from "react-router-dom";
 
 const AuthForm = () => {
+  const history = useHistory();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true);
+
+    let url;
+
+    if (isLogin) {
+      url = `${process.env.REACT_APP_REQUEST_URL}/auth/signin`;
+    } else {
+      url = `${process.env.REACT_APP_REQUEST_URLL}/auth/signup`;
+    }
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      setIsLoading(false);
+      const data = await response.json();
+
+      if (response.ok) {
+        const expirationTime = new Date(new Date().getTime() + 1 * 60 * 1000);
+        authCtx.login(data.access_token, expirationTime.toISOString());
+        history.replace("/");
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>...Loading</p>}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,24 +1,27 @@
-import { Link } from 'react-router-dom';
-
-import classes from './MainNavigation.module.css';
+import { Link ,useHistory} from "react-router-dom";
+import { useContext } from "react";
+import classes from "./MainNavigation.module.css";
+import AuthContext from "../../store/auth-context";
 
 const MainNavigation = () => {
+  const history = useHistory();
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+  const logoutHandler =()=>{
+    authCtx.logout();
+    history.replace('/');
+    
+  } 
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          <li>{!isLoggedIn && <Link to="/auth">Login</Link>}</li>
+          <li>{isLoggedIn && <Link to="/profile">Profile</Link>}</li>
+          <li>{isLoggedIn && <button onClick={logoutHandler}>Logout</button>}</li>
         </ul>
       </nav>
     </header>
